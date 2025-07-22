18d1c950d6028ea1d6710a3b2d50cf8dd7b00783
Sri Navya Kota <kota@Sris-MacBook-Pro.local>
2025-07-22 23:17:53 +0530

    5. The Spring IoC Container

--- src/main/java/com/codeWithMosh/store/StoreApplication.java
+++ src/main/java/com/codeWithMosh/store/StoreApplication.java
@@ -2,15 +2,20 @@ package com.codeWithMosh.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(StoreApplication.class, args);
-		OrderService orderService = new OrderService(new PayPalPaymentService());
-		orderService.placeOrder();
-		orderService.setPaymentService(new StripePaymentService());
+
+		// ApplicationContext is the IOC for Spring-Boot-Application
+		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+
+		// Method to get the Bean of OrderService Class.
+		// This is going to throw an error as we have not yet configured the OrderService.class as a Bean
+		var orderService = context.getBean(OrderService.class);
+
 		orderService.placeOrder();
 	}
 
