6607a15d2b1a121ee56f519382e2eb2784d48f31
Sri Navya Kota <kota@Sris-MacBook-Pro.local>
2025-07-24 23:33:10 +0530

    Lets start understanding the application.properties file

--- src/main/java/com/codeWithMosh/store/OrderService.java
+++ src/main/java/com/codeWithMosh/store/OrderService.java
@@ -2,7 +2,6 @@ package com.codeWithMosh.store;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Service
--- src/main/java/com/codeWithMosh/store/StoreApplication.java
+++ src/main/java/com/codeWithMosh/store/StoreApplication.java
@@ -13,10 +13,6 @@ public class StoreApplication {
 		// ApplicationContext is the IOC for Spring-Boot-Application
 		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 
-		// If we mark both PayPalPaymentService.java and StripePaymentService.java both as @Service we will get an error saying Bean is not able to resolve. "Parameter 0 of constructor in com.codeWithMosh.store.OrderService required a single bean, but 2 were found:"
-		// To overcome this, we can use 2 different annotations
-		// @Primary -> When multiple beans are present, primary bean will be used always.
-		// In case if some class doesn't want to go with @Primary bean, we can set a @Qualifier("service_name") bean.
 		var orderService = context.getBean(OrderService.class);
 
 		orderService.placeOrder();
