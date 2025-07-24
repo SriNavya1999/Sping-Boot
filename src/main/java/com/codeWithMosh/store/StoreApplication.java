200721ac503544a8d8591e00906ba024c5596dad
Sri Navya Kota <kota@Sris-MacBook-Pro.local>
2025-07-25 00:53:30 +0530

    12. Bean Scopes

--- src/main/java/com/codeWithMosh/store/StoreApplication.java
+++ src/main/java/com/codeWithMosh/store/StoreApplication.java
@@ -12,7 +12,21 @@ public class StoreApplication {
 
 		// ApplicationContext is the IOC for Spring-Boot-Application
 		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-		context.getBean(HeavyResource.class);
+
+		// Bean Scopes: There are 4 main diff Scopes for Beans in Spring
+		// 1. Singleton: Only one instance of the bean is created and shared across the application.
+		// 2. Prototype: A new instance of the bean is created every time it is requested.
+		// 3. Request: A new instance of the bean is created for each HTTP request. And is destroyed at the end of the request.
+		// 4. Session: A new instance of the bean is created for each HTTP session. And is destroyed at the end of the session.
+
+		// HeavyResource is a bean created only once as it is a singleton bean.
+		HeavyResource heavyResource = context.getBean(HeavyResource.class);
+		HeavyResource heavyResource1 = context.getBean(HeavyResource.class);
+
+		// OrderService is marked as a prototype bean in AppConfig.java. So, orderService1 and orderService2 are different instances.
+		OrderService orderService1 = context.getBean(OrderService.class);
+		OrderService orderService2 = context.getBean(OrderService.class);
+
 
 //		System.out.println("Spring Boot Application Started");
 //
