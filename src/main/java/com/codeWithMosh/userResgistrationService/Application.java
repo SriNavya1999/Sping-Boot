937fdcfaecc6e1f1d5f2f0b26c1bad66b5d2f25f
Sri Navya Kota <kota@Sris-MacBook-Pro.local>
2025-07-26 23:52:05 +0530

    4. Setting Up Spring Data JPA

--- src/main/java/com/codeWithMosh/userResgistrationService/Application.java
+++ src/main/java/com/codeWithMosh/userResgistrationService/Application.java
@@ -8,8 +8,5 @@ import org.springframework.context.ApplicationContext;
 public class Application {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class, args);
-        UserService userService = context.getBean(UserService.class);
-        userService.register(new User(1, "John Doe", "john.doe@gmail.com", "password123"));
-        userService.register(new User(1, "John Doe", "john.doe@gmail.com", "password123"));
     }
 }
