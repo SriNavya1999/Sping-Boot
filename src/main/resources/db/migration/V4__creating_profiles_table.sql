9e1d02e78fd4302f4232291dc74236775fcfb1f3
Sri Navya Kota <kota@Sris-MacBook-Pro.local>
2025-07-31 21:23:30 +0530

    Modified tags and user_tags tables - remved the constraint - felt that its useless

--- src/main/resources/db/migration/V4__creating_profiles_table.sql
+++ src/main/resources/db/migration/V4__creating_profiles_table.sql
@@ -6,7 +6,6 @@ create table profiles
     phone_number   varchar(15)              null,
     date_of_birth  date                     null,
     loyalty_points int unsigned default '0' null,
-    constraint profiles_ibfk_1
-        foreign key (id) references users (id)
+    foreign key (id) references users (id)
 );
 
--- src/main/resources/db/migration/V5__creating_tags_and_user_tags_tables.sql
+++ src/main/resources/db/migration/V5__creating_tags_and_user_tags_tables.sql
@@ -9,10 +9,8 @@ create table user_tags
 (
     user_id bigint not null,
     tag_id  int    not null,
-    constraint user_tags_ibfk_1
-        foreign key (user_id) references users (id)
+    foreign key (user_id) references users (id)
             on delete cascade,
-    constraint user_tags_ibfk_2
-        foreign key (tag_id) references tags (id)
+    foreign key (tag_id) references tags (id)
             on delete cascade
 );
